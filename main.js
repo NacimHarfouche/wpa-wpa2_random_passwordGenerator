c83ebef97719abd677209719feae7bc8aa96d058
NacimHarfouche <nacim_4@hotmail.fr>
2019-02-23 01:07:18 +0100

    lighten & translate code

--- main.js
+++ main.js
@@ -1,61 +1,38 @@
+// array that contains the characters
 const tab = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "%", ",", "!", "-", "#", "+", "&", "/"];
-let spanElts = document.querySelectorAll("span");
+// button
 let buttonElt = document.querySelector("button");
-let symbole = new Number();
+
+// stock random number
+let symbol = new Number();
 let zeroOrOne = new Number();
 
-// genere des nombres aleatoire
+// stock the chosen password length
+let chooseLength;
+
+// display the generate pass in the paragraph (DOM)
+let paraElt = document.getElementById("para");
+
+// stock the pass before he is push in the para
+let saveIt = new String();
+
+// generates a random number
 function randomNumber(tab) {
-    symbole = Math.floor(Math.random() * tab.length);
+    symbol = Math.floor(Math.random() * tab.length);
     zeroOrOne = Math.floor(Math.random() * 2);
 }
 
-
-
-let chooseLength;
-let interval;
-let paraElt = document.getElementById("para");
-let saveIt;
-/*function showIt() {
-    
-    console.log(document.getElementById("textinput").value.toLocaleLowerCase());
-}
-
-function affichage() {
-    interval = setInterval(showIt, 1000);
-}
-affichage();*/
-
-////////////////////////////////////////////////////////
-/*buttonElt.addEventListener("click", function() {
-    chooseLength = document.getElementById("textinput").value;
-    saveIt = "";
-    for (let i = 0; i < chooseLength; i++) {
-        randomNumber(tab);
-        if (zeroOrOne === 1 && symbole < 26) {
-            let upper = tab[symbole].toUpperCase();
-            paraElt.textContent += upper;
-            saveIt += upper;
-        } else {
-            paraElt.textContent += tab[symbole];
-            saveIt += tab[symbole];
-        }
-    }
-    console.log(tab[chooseLength]);
-    console.log(saveIt);
-});*/
-/////////////////////////////////////////////////////
-
+// on click it generate a pass
 buttonElt.addEventListener("click", function() {
+    // get the value of the chosen length
     chooseLength = document.getElementById("textinput").value;
-    saveIt = "";
     for (let i = 0; i < chooseLength; i++) {
         randomNumber(tab);
-        if (zeroOrOne === 1 && symbole < 26) {
-            let upper = tab[symbole].toUpperCase();
+        if (zeroOrOne === 1 && symbol < 26) {
+            let upper = tab[symbol].toUpperCase();
             saveIt += upper;
         } else {
-            saveIt += tab[symbole];
+            saveIt += tab[symbol];
         }
     }
     paraElt.textContent = saveIt;
